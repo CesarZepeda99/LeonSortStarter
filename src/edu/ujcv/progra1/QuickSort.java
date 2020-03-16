fefbdfc3e46e1e046c4dc48f7f74c6fe083298c1
CesarZepeda99 <cesareduardozepeda@gmail.com>
2020-03-16 17:12:16 -0600

    QUICKSORT PRIMERA PARTE, CREACION DE INDICADORES IZQUIERDA Y DERECHA Y CREACION DE PIVOTE Y AUXILIAR

---
+++ src/edu/ujcv/progra1/QuickSort.java
@@ -0,0 +1,18 @@
+package edu.ujcv.progra1;
+
+public class QuickSort {
+
+    public int[] quicksort(int num[]) {
+         return quicksort(num, 0, num.length-1);
+    }
+    public int [] quicksort(int num[], int izq,int der){
+        if (izq>=der)
+            return num;
+        int i=izq,d=der;
+        if(izq!=der){
+            int pivote;
+            int aux;
+            pivote = izq;
+        }
+    }
+}
