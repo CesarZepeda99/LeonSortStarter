e40acf86f786ed910b9bdab2ba7d45f42c036993
CesarZepeda99 <cesareduardozepeda@gmail.com>
2020-03-18 13:16:50 -0600

    Quicksort ya funciona y ordena

--- src/edu/ujcv/progra1/QuickSort.java
+++ src/edu/ujcv/progra1/QuickSort.java
@@ -1,35 +1,46 @@
 package edu.ujcv.progra1;
 
-public class QuickSort {
+public class QuickSort implements SortTester {
+    public long sort(int[] array) {
+        long start = System.nanoTime();
+        quicksort(array);
+
+        long end = System.nanoTime();
+
+        return end - start;
+    }
 
     public int[] quicksort(int num[]) {
-         return quicksort(num, 0, num.length-1);
+        return quicksort(num,0,num.length - 1);
     }
-    public int [] quicksort(int num[], int izq,int der){
-        if (izq>=der)
+
+    public int[] quicksort(int num[], int izq, int der) {
+        if (izq >= der)
             return num;
-        int i=izq,d=der;
-        if(izq!=der){
+        int i = izq, d = der;
+        if (izq != der) {
             int pivote;
             int aux;
             pivote = izq;
-            while (izq!=der)
-                (imprimeArreglo(num);
-                while(num[izq]<num[pivote] && izq<der)
+            while (izq != der) {
+
+                while (num[izq] < num[pivote] && izq < der)
                     der--;
-                while(num[izq]<num[pivote] && izq<der)
+                while (num[izq] < num[pivote] && izq < der)
                     izq++;
 
-                if (der!=izq){
+                if (der != izq) {
                     aux = num[der];
-                    num[der]= num[izq];
-                    num[izq]=aux;
+                    num[der] = num[izq];
+                    num[izq] = aux;
                 }
-                if (izq==der){
-                    quicksort(num,i,izq-1);
-                    quicksort(num,izq+1,d);
-                }
-
-        }
+            }
+            if (izq == der) {
+                quicksort(num, i, izq - 1);
+                quicksort(num, izq + 1, d);
+            }
+        } else
+            return num;
+        return num;
     }
 }
